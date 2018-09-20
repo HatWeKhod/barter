550b9d5318bb2099a6cda49e97f937961352175c
Biswajit Manna <biswajit.manna@shyamfuture.com>
2018-09-20 18:26:23 +0530

    QA fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ const routes: Routes = [
   { path: 'login', loadChildren: './login/login.module#LoginModule' },
   { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] }
 ];
-//{ path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
